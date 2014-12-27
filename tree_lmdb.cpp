1cb5e0f688bfa79e37c683929f277d510bd99edd
Filip Ginter <ginter@cs.utu.fi>
2014-12-27 14:21:15 +0200

    support for tree/tset deserialization

--- tree_lmdb.cpp
+++ tree_lmdb.cpp
@@ -148,19 +148,81 @@ int LMDB::store_key_tree(unsigned int tree_id, void *key_data, int key_size) {
 }
 
 //binary search
-int b_search(uint32_t what, uint32_t *beg, uint32_t *end) {
+//Returns pointer to the occurrence of what in the array [beg,...,end] if found, or NULL otherwise
+uint32_t *binary_search(uint32_t what, uint32_t *beg, uint32_t *end) {
     uint32_t *mid;
     do {
-	mid=beg+(end-beg)/2;
+	mid=beg+(end-beg)/2; //pointer to the middle
 	if (*mid == what) {
-	    return 1;
+	    return mid;
 	}
-	else if (*mid < what) {
-	    beg=mid;
+	else if (*mid < what) { //start one to the right of mid
+	    beg=mid+1;
 	}
-	else {
-	    end=mid;
+	else { //*mid > what, end one to the left of mid
+	    end=mid-1;
 	}
-    } while (beg!=end);
+    } while (beg<=end);
+    return NULL;
+}
+
+void Tree::deserialize(void *serialized_data) {
+    tree_length=((uint16_t *)serialized_data)[0];
+    array_length=(tree_length/sizeof(uint64_t)+1); //how many uint64_t's are needed to store the set?
+    set_count=*(&tree_length+1);
+    map_count=*(&tree_length+2);
+    set_indices=(uint32_t *)(void*)(&map_count+1);
+    map_indices=set_indices+set_count;
+    map_lengths=(uint16_t *)(void*)(map_indices+map_count);
+    set_data=(uint64_t *)(void *)(map_lengths+map_count);
+    serialized_map_data=(void*)(set_data+array_length*set_count);
+    map_data_pointer_byte_offsets[0]=0; //serialized maps are of varying lengths - accumulate here byte offset for their data
+    for (int i=1; i<map_count; i++) {
+	map_data_pointer_byte_offsets[i]=map_data_pointer_byte_offsets[i-1]+map_lengths[i-1];
+    }
+    void *zipped_block=(void*) ((map_count==0) ? serialized_map_data : ((char *)serialized_map_data)+map_data_pointer_byte_offsets[map_count-1]+map_lengths[map_count-1]);
+    zipped_tree_text_length=*((uint16_t *)(zipped_block));
+    zipped_tree_text=(void*)((char*)zipped_block+sizeof(uint16_t));
+}
+
+int Tree::fill_sets(void **set_pointers, uint32_t *indices, unsigned char *set_types, unsigned char *optional, unsigned int count) {
+    //set_pos -> index into set_pointers etc, runs in range [0,count)
+    for (int set_pos=0; set_pos<count; set_pos++) {
+	if (set_types[set_pos]==1) { //we are looking for a tset
+	    TSet *tset=(TSet *) set_pointers[set_pos]; //current set
+	    uint32_t* p=binary_search(indices[set_pos],set_indices,set_indices+set_count-1); //return NULL, or a pointer to indices
+	    if (p==NULL && !optional[set_pos]) { //didn't find it and it was compulsory...
+		return 1;
+	    }
+	    else if (p==NULL) { //didn't find it and it was not compulsory
+		tset->set_length(tree_length);
+		tset->erase();
+	    }
+	    else {
+		//got a set!
+		int set_index=p-set_indices; //binary search returns a pointer, so p-set_indices is the index of the item found
+		tset->deserialize(tree_length,(const void*)&set_data[set_index*array_length]);
+	    }
+	}
+	else if (set_types[set_pos]==2) { //we are looking for an array
+	    TSetArray *tsetarray=(TSetArray *) set_pointers[set_pos]; //current set
+	    uint32_t* p=binary_search(indices[set_pos],map_indices,map_indices+map_count-1); //return NULL, or a pointer to indices
+	    if (p==NULL && !optional[set_pos]) { //didn't find it and it was compulsory...
+		return 1;
+	    }
+	    else if (p==NULL) { //didn't find it and it was not compulsory
+		tsetarray->set_length(tree_length);
+		tsetarray->erase();
+	    }
+	    else {
+		//got an array!
+		int set_index=p-map_indices; //binary search returns a pointer, so p-set_indices is the index of the item found
+		tsetarray->deserialize(tree_length,(void*)((char*)serialized_map_data+map_data_pointer_byte_offsets[set_index]),map_lengths[set_index]);
+	    }
+	}
+    }
     return 0;
 }
+		
+		    
+		
--- tree_lmdb.h
+++ tree_lmdb.h
@@ -2,6 +2,10 @@
 #define __tree_lmdb_h__
 
 #include <lmdb.h>
+#include "setlib/tset.h"
+using namespace tset;
+
+uint32_t *binary_search(uint32_t what, uint32_t *beg, uint32_t *end);
 
 class LMDB {
 public:
@@ -31,7 +35,20 @@ public:
     uint16_t map_count; //Number of maps stored in map_indices and maps
     uint32_t *set_indices; //Index for every set (i.e. what kind of set is it?
     uint32_t *map_indices; //Index for every map (i.e. what kind of map is it?
-    uint64_t *setdata; //Set data arrays for the sets in set_indices
+    uint16_t *map_lengths; //For every map, the length (in bytes) of the data it stores
+    uint64_t *set_data; //Set data arrays for the sets in set_indices
+    void *serialized_map_data; //Serialized array for the maps in map_indices
+    uint16_t zipped_tree_text_length; //length of the zipped tree data
+    void *zipped_tree_text;  //zipped tree data
+    
+    int array_length;
+    uint16_t *map_data_pointer_byte_offsets;
+
+    void deserialize(void *serialized_data);
+
+    //Fill count many sets into set_pointers
+    //set_types: 1:tset  2:tsetarray
+    int fill_sets(void **set_pointers, uint32_t *indices, unsigned char *set_types, unsigned char *optional, unsigned int count);
 };
     
 
