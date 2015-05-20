0f3897cb1ce7f5846d5d1789943ef69cea63ccc6
mjluot <mjluot@utu.fi>
2015-05-20 15:29:49 +0300

    error report

--- webui/serve.py
+++ webui/serve.py
@@ -12,13 +12,12 @@ import flask
 
 # Name of JSON containing information on available DBs.
 CORPORA_FILENAME = 'corpora.json'
-
+CORPORA_LISTFILE = 'available_corpora.json'
 # App settings. IMPORTANT: set DEBUG = False for publicly accessible
 # installations, the debug mode allows arbitrary code execution.
 DEBUG = False
-HOST = '0.0.0.0'
-PATH = '/'
-PORT = 5042
+HOST = 'localhost'
+PORT = 80
 STATIC_PATH = '/static'
 DB_PARAMETER = 'db'
 QUERY_PARAMETER = 'search'
@@ -32,18 +31,28 @@ DBS_PLACEHOLDER = '{{ OPTIONS }}'
 DB_PLACEHOLDER = '{{ DBNAME }}'
 CONTENT_START = '<!-- CONTENT-START -->'
 CONTENT_END = '<!-- CONTENT-END -->'
+ERROR_PLACEHOLDER = '{{ ERROR }}'
 
 # Visualization wrapping
 visualization_start = '<pre><code class="conllu">'
 visualization_end = '</code></pre>'
 
-def server_url(host=HOST, port=PORT, path=PATH):
-    url = '%s:%d%s' % (host, port, path)
+def server_url(host=HOST, port=PORT):
+    url = host#:%d' % (host, port)
     if not url.startswith('http://'):
         url = 'http://' + url # TODO do this properly
     return url
 
 def load_corpora(filename=CORPORA_FILENAME):
+    try:
+        with open(filename) as f:
+            return json.loads(f.read())
+    except Exception, e:
+        print 'Failed to load data on corpora from', filename
+        raise
+
+def load_corpora_list(filename=CORPORA_LISTFILE):
+
     try:
         with open(filename) as f:
             return json.loads(f.read())
@@ -69,7 +78,7 @@ def perform_query(dbname, query):
     args = [query_cmd, '-d', dbs, '-m', '100', query.encode('utf-8')]
     p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (out, err) = p.communicate()
-    return out
+    return out, err
 
 def get_index(fn=INDEX_TEMPLATE):
     with codecs.open(fn, encoding='utf-8') as f:
@@ -80,30 +89,34 @@ def get_template(fn=RESULT_TEMPLATE):
         return f.read()
 
 def render_dbs(selected):
-    corpora = load_corpora()
+    corpora = load_corpora_list()
+    print corpora
     options = []
     for name in corpora:
         s = ' selected="selected"' if name == selected else ''
         options.append('<option value="%s"%s>%s</option>' % (name, s, name))
     return '\n'.join(options)
 
-def default_db():
-    # choose at random
-    return load_corpora().keys()[0]
-
-def fill_template(template, content='', dbname='', query=''):
+def fill_template(template, content='', error='', dbname='', query=''):
     # TODO: use jinja
-    if not dbname:
-        dbname = default_db()
+    if dbname is None:
+        dbname = ''
     assert CONTENT_START in template
     assert CONTENT_END in template
     header = template[:template.find(CONTENT_START)]
     trailer = template[template.find(CONTENT_END):]
+    print type(header), type(content), type(trailer)
     filled = header + content + trailer
-    filled = filled.replace(SERVER_URL_PLACEHOLDER, server_url().rstrip('/'))
-    filled = filled.replace(QUERY_PLACEHOLDER, query.replace('"', '&quot;')
+    filled = filled.replace(SERVER_URL_PLACEHOLDER, server_url())
+    t_q = query[:]
+    t_q = t_q.replace('"', '&quot;')
+    filled = filled.replace(QUERY_PLACEHOLDER, t_q)
     filled = filled.replace(DBS_PLACEHOLDER, render_dbs(dbname))
     filled = filled.replace(DB_PLACEHOLDER, dbname)
+    if len(error) < 1:
+        filled = filled.replace(ERROR_PLACEHOLDER, '')
+    else:
+        filled = filled.replace(ERROR_PLACEHOLDER, '<div style="background-color:black; color:white; padding:20px;"><p>' + str(error) + '</p></div>')
     return filled
 
 app = flask.Flask(__name__, static_url_path=STATIC_PATH)
@@ -111,17 +124,24 @@ app = flask.Flask(__name__, static_url_path=STATIC_PATH)
 def query_and_fill_template(dbname, query):
     template = get_template()
     try:
-        results = perform_query(dbname, query)
+        results, error = perform_query(dbname, query)
     except Exception, e:
         return "Internal error: %s" % (str(e))
     # plug in separate visualizations to allow for progressive loading
+    #error = []
+    if 'raise' not in error:
+        error = ''
+    else:
+        if 'ExpressionError:' in error:
+            error = error.split('ExpressionError:')[-1]
+
     visualizations = []
     for block in results.split('\n\n')[:-1]:
         block = block.decode('utf-8')
         visualizations.append(visualization_start +
                               block + '\n\n' +
                               visualization_end)
-    return fill_template(template, ''.join(visualizations), dbname, query)
+    return fill_template(template, ''.join(visualizations), error, dbname, query)
 
 def _types(db):
     symbols = get_database_symbols(db)
@@ -132,9 +152,11 @@ def _types(db):
     for key, value in symbols.iteritems():
         for item in value:
             groups[item[0]].append((key, item))
+
     # sort each group
     for group in groups.keys():
         groups[group].sort()
+
     # convert each group to HTML
     output = {}
     for group in groups:
