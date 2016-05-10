6c0dcba999e54839a2bfd412ac99c950e28fe4c8
Filip Ginter <ginter@cs.utu.fi>
2016-05-10 16:02:22 +0300

    UTF8 bugfix for api query

--- webui/serve_webapi.py
+++ webui/serve_webapi.py
@@ -10,18 +10,17 @@ app = flask.Flask(__name__)
 
 ABSOLUTE_RETMAX=100000
 
-with open("corpora.json","rt") as f:
-    corpora=json.load(f)
-
 @app.route("/",methods=["GET"])
 def run_query():
-    if "q" not in flask.request.args:
-        return flask.Response("<html><body><ul><li>q: the query to run</li><li>corpora: corpus,corpus,corpus</li><li>max: max number of results (will be capped at 100K by us)</li><li>dl: set headers to offer file for download</li></body></html>")
-    retmax=int(flask.request.args.get("max",1000)) #default is 1000
+    with open("corpora.json","rt") as f:
+        corpora=json.load(f)
+    if "search" not in flask.request.args:
+        return flask.Response("<html><body><ul><li>search: the query to run</li><li>db: corpus,corpus,corpus</li><li>retmax: max number of results (will be capped at 100K by us)</li><li>dl: set headers to offer file for download</li></body></html>")
+    retmax=int(flask.request.args.get("retmax",1000)) #default is 1000
     retmax=min(retmax,ABSOLUTE_RETMAX)
 
     dbs=[]
-    for corpus in flask.request.args.get("corpora","").split(","):
+    for corpus in flask.request.args.get("db","").split(","):
         path=corpora.get(corpus)
         if path is None:
             continue
@@ -29,7 +28,7 @@ def run_query():
     dbs=sorted(dbs)
 
     def generate():
-        args=["python","query.py","-m",str(retmax),flask.request.args["q"],"--dblist"]+dbs
+        args=["python","query.py","-m",str(retmax),flask.request.args["search"].encode("utf-8"),"--dblist"]+dbs
         print >> sys.stderr, "Running", args
         proc=sproc.Popen(args=args,cwd="..",stdout=sproc.PIPE)
         for line in proc.stdout:
@@ -42,6 +41,6 @@ def run_query():
 if __name__=="__main__":
     host='0.0.0.0'
     port=45678
-    app.run(host=host, port=port, debug=True, use_reloader=True)
+    app.run(host=host, port=port, debug=False, use_reloader=True)
 
 
